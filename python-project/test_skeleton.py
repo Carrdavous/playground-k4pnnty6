de51e9e78eccdcbd032d2a8301f2d0fa8b9c0641
David Carrera <62470722+Carrdavous@users.noreply.github.com>
2025-11-25 10:19:48 +0100

    Update test_skeleton.py

--- python-project/test_skeleton.py
+++ python-project/test_skeleton.py
@@ -9,24 +9,10 @@ def test_est_divisible_par_3():
         assert test2 == True, "39 est divisible par 39: {test2}... Attentu: True"
         success()
 
-        if sum_builtin_used:
-            send_msg("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ")
-            send_msg("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ")
-            send_msg("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .")
-            send_msg("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　")
-            send_msg("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ")
-            send_msg("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ")
-            send_msg("My personal Yoda, you are. 🙏", "_)_/LI")
-        else:
-            send_msg("Kudos 🌟", "Did you know that you could use the sum function? Try it!")
-            send_msg("Kudos 🌟", "")
-            send_msg("Kudos 🌟", "galaxies = [37, 3, 2]")
-            send_msg("Kudos 🌟", "total_stars = sum(galaxies)  # 42")
+        send_msg("Félicitations !", "Vous avez répondu correctement à l'énoncé")
     except AssertionError as e:
         fail()
-        send_msg("Oops! 🐞", e)
-        send_msg("Hint 💡", "Did you properly accumulate all stars into 'total_stars'? 🤔")
-
+        send_msg("Oups! 🐞", e)
 
 if __name__ == "__main__":
     test_something()
