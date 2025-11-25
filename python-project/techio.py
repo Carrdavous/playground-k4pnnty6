5250e6d5437c93796ab39d5803738693e5808b49
David Carrera <62470722+Carrdavous@users.noreply.github.com>
2025-11-25 10:06:00 +0100

    Create techio.py

---
+++ python-project/techio.py
@@ -0,0 +1,10 @@
+def send_msg(channel, msg):
+    print(f'TECHIO> message --channel "{channel}" "{msg}"')
+
+
+def success():
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
